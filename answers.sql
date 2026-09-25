6db712ded28784fce28867eb6725907833b30933
1U25CS005-ADITH. R <adithramesh76@gmail.com>
2026-09-25 12:39:30 +0530

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,1 +1,54 @@
+CREATE DATABASE ADITH22;
+USE ADITH22;
+	CREATE TABLE Department (
+    DepartmentID INT PRIMARY KEY,
+    DepartmentName VARCHAR(50)
+);
+CREATE TABLE Faculty (
+    FacultyID INT PRIMARY KEY,
+    FacultyName VARCHAR(50),
+    DepartmentID INT,
+    FOREIGN KEY (DepartmentID) REFERENCES Department(DepartmentID)
+);
+CREATE TABLE Course (
+    CourseID INT PRIMARY KEY,
+    CourseName VARCHAR(50),
+    FacultyID INT,
+    FOREIGN KEY (FacultyID) REFERENCES Faculty(FacultyID)
+);
+CREATE TABLE Student (
+    StudentID INT PRIMARY KEY,
+    StudentName VARCHAR(50),
+    CourseID INT,
+    FOREIGN KEY (CourseID) REFERENCES Course(CourseID)
+);
+INSERT INTO Department VALUES
+(1, 'Computer Science'),
+(2, 'Information Technology');
+INSERT INTO Faculty VALUES
+(101, 'Kumar', 1),
+(102, 'Priya', 1),
+(103, 'Ravi', 2);
+INSERT INTO Course VALUES
+(201, 'Java', 101),
+(202, 'DBMS', 102),
+(203, 'Python', 103);
+INSERT INTO Student VALUES
+(1001, 'Arun', 201),
+(1002, 'Divya', 202),
+(1003, 'Karthik', 203);
+SELECT * FROM Department;
+SELECT * FROM Faculty;
+SELECT * FROM Course;
+SELECT * FROM Student;
 
+SELECT
+    s.StudentID,
+    s.StudentName,
+    c.CourseName,
+    f.FacultyName,
+    d.DepartmentName
+FROM Student s
+JOIN Course c ON s.CourseID = c.CourseID
+JOIN Faculty f ON c.FacultyID = f.FacultyID
+JOIN Department d ON f.DepartmentID = d.DepartmentID;
